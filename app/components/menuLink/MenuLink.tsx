293803260b5e7b1bbb4ed19e761c4e6b5ae0b723
CristinaJung <cristijung@gmail.com>
2025-09-25 17:33:39 -0300

    links

---
+++ app/components/menuLink/MenuLink.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link';
+import { LinkProps } from '@/app/types/types';
+
+
+export function CustomLink({ href, children, className }: LinkProps) {
+  return (
+    <Link
+      href={href}
+      className={`
+        inline-flex items-center justify-center
+        rounded-lg
+        bg-blue-600 px-6 py-3 text-lg font-semibold text-white
+        shadow-lg
+        transition-all duration-300
+        hover:scale-105 hover:bg-blue-700 hover:shadow-xl
+        focus:outline-none focus:ring-4 focus:ring-blue-500/50
+        ${className || ''}
+      `}
+    >
+      {children}
+    </Link>
+  );
+}
